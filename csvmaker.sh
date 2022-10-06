09ff667ce7522ac33726dd86aea61cc473ec0bd5
ulsin <ulriksin@gmail.com>
2022-10-06 22:32:45 +0200

    📦 NEW: images in diagram

--- csvmaker.sh
+++ csvmaker.sh
@@ -1,1 +1,3 @@
-echo "author_name, time_sec, subject" > log.csv && git --no-pager log --pretty=format:%an,%at,%s >> log.csv
+echo "author_name, time_sec, subject" > log.csv
+git --no-pager log --pretty=format:%an,%at,%s >> log.csv
+
